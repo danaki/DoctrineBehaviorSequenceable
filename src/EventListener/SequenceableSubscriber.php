e28a0125e92cb807be52e651ed7f0ce1f5f90876
Falko Matthies <falko.matthies@fincallorca.de>
2017-05-24 14:53:17 +0200

    Added method _backupDeletedEntity() to backup soft-deleted entities

--- src/EventListener/SequenceableSubscriber.php
+++ src/EventListener/SequenceableSubscriber.php
@@ -100,7 +100,10 @@ class SequenceableSubscriber implements EventSubscriber
 //			}
 //		}
 
+		// Attention: Get all entities before looping over all entities
+		// because in the loops entities are added to the schedules (updates and insertions)! 
 		$entities_to_update = $unit_of_work->getScheduledEntityUpdates();
+		$entities_to_insert = $unit_of_work->getScheduledEntityInsertions();
 
 		// iterate over all entities
 		foreach( $entities_to_update as $_entity )
@@ -120,6 +123,30 @@ class SequenceableSubscriber implements EventSubscriber
 
 			// create a backup of the whole row
 			$this->_backupUpdateEntity($args->getEntityManager(), $_entity);
+		}
+
+		foreach( $entities_to_insert as $_entity )
+		{
+
+			$class_name = get_class($_entity);
+
+			/** @var Sequenceable $_entity */
+			if( !SequenceableHelper::isSequenceable($entity_manager->getClassMetadata($class_name)) )
+			{
+				continue;
+			}
+
+			if( !array_key_exists($class_name, $this->_classMetadataContainer) )
+			{
+				throw MappingException::unknownSequenceableEntity($class_name);
+			}
+
+			if( trait_exists('\Knp\DoctrineBehaviors\Model\SoftDeletable\SoftDeletable')
+				&& in_array(\Knp\DoctrineBehaviors\Model\SoftDeletable\SoftDeletable::class, class_uses($_entity))
+			)
+			{
+				$this->_backupDeletedEntity($entity_manager, $_entity);
+			}
 		}
 	}
 
@@ -169,6 +196,78 @@ class SequenceableSubscriber implements EventSubscriber
 
 		// compute change set, see {@link http://stackoverflow.com/questions/9583058/inserting-element-in-doctrine-listener}
 		$entity_manager->getUnitOfWork()->computeChangeSet($meta_class, $backup_entity);
+	}
+
+	/**
+	 * Inserts a backup of the current soft-deleted entity. If no soft-deleted entity exists, no backup will be inserted :-)
+	 *
+	 * To remain the current id at the new entity, the backup replaces the $new_entity, whereas the new entity updates the values of the
+	 * (existing) soft-deleted entity.
+	 *
+	 * @param EntityManager $entity_manager
+	 * @param Sequenceable  $new_entity
+	 */
+	protected function _backupDeletedEntity(EntityManager $entity_manager, $new_entity)
+	{
+		$class_name = get_class($new_entity);
+
+		$meta_class = $this->_classMetadataContainer[ $class_name ]->getClassMetadata();
+
+		// get current deleted entity
+		$deleted_entity = $this->_classMetadataContainer[ $class_name ]->getDeletedEntity($entity_manager, $new_entity);
+
+		// if no deleted entity exists, no backup is needed
+		if( is_null($deleted_entity) )
+		{
+			return;
+		}
+
+		$unit_of_work = $entity_manager->getUnitOfWork();
+
+		// 1. save backup as "new" entity to remain the id at the right new entity
+
+		// get change set to store new values in "old" soft-deleteable entity
+		$change_set = $entity_manager->getUnitOfWork()->getEntityChangeSet($new_entity);
+
+		// overwrite new entity with the "old" values to save a backup as a new entity (with new id)
+		foreach( $meta_class->getFieldNames() as $_field_name )
+		{
+
+			if( in_array($_field_name, $meta_class->getIdentifierColumnNames()) )
+			{
+				continue;
+			}
+
+			$meta_class->setFieldValue($new_entity, $_field_name, $meta_class->getFieldValue($deleted_entity, $_field_name));
+		}
+
+		// update sequence number
+		$new_entity->setSequence(
+			$this->_classMetadataContainer[ $class_name ]->getBackupSequenceNo($entity_manager, $deleted_entity)
+		);
+
+		// recompute changeset, see {@link http://stackoverflow.com/questions/9583058/inserting-element-in-doctrine-listener}
+		$unit_of_work->recomputeSingleEntityChangeSet($meta_class, $new_entity);
+
+		// 2. save new entity as update of existing soft-deleted entity
+
+		// reset new values to soft-deleted entity
+		foreach( $change_set as $_field_name => $_value )
+		{
+			$meta_class->setFieldValue($deleted_entity, $_field_name, $_value[ 1 ]);
+		}
+
+		// but the remove id
+		// attention: generatorType must be {@see ClassMetadata::GENERATOR_TYPE_IDENTITY} if removing the id(s) 
+		foreach( $meta_class->getIdentifierColumnNames() as $_identifier_column_names )
+		{
+			$meta_class->setFieldValue($deleted_entity, $_identifier_column_names, null);
+		}
+
+		// also update existing soft-deleted entity which becomes non soft-deleted
+		$unit_of_work->scheduleForUpdate($deleted_entity);
+		// recompute changeset, see {@link http://stackoverflow.com/questions/9583058/inserting-element-in-doctrine-listener}
+		$unit_of_work->recomputeSingleEntityChangeSet($meta_class, $deleted_entity);
 	}
 
 }
